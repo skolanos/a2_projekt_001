f9dafd33b3bbbed30b4632cf4de03265ca144c37
skolanos <skolanos77@gmail.com>
2017-02-23 21:22:49 +0100

    Prace nad obsługą koszyka

--- client/app/components/cart-list.component.ts
+++ client/app/components/cart-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { CartService } from '../services/cart.service';
 
+declare var BigDecimal: any;
+declare var RoundingMode: any;
+
 @Component({
 	moduleId: module.id,
 	selector: 'cart-list-component',
@@ -11,12 +14,14 @@ export class CartListComponent implements OnInit {
 	private processing: boolean;
 	private messages: string[];
 	private items: any[];
+	private totalValue: string;
 	private selectedItem: any;
 
 	constructor(private cartService: CartService) {
 		this.processing = false;
 		this.messages = [];
 		this.items = [];
+		this.totalValue = '0.00';
 		this.selectedItem = undefined;
 	}
 	ngOnInit(): void {
@@ -28,6 +33,7 @@ export class CartListComponent implements OnInit {
 			this.processing = false;
 			if (value.status === 200) {
 				this.items = value.data;
+				this.computeTotalValue();
 			}
 			else {
 				this.messages.push(value.message);
@@ -40,5 +46,60 @@ export class CartListComponent implements OnInit {
 	}
 	selectItemClick(item: any): void {
 		this.selectedItem = item;
+	}
+	private isValidInt(value: string): boolean {
+		let res: RegExpMatchArray = undefined;
+
+		if (value) {
+			res = value.match(/^[-+]?\d+$/);
+		}
+
+		return (res && (res.length > 0)) ? true: false;
+	}
+	private computeTotalValue() {
+		let currentValue = '0';
+		let validAmount = true;
+		for (let i = 0; i < this.items.length; i += 1) {
+			if (this.isValidInt(String(this.items[i].ko_ile))) {
+				let amountBD: any = new BigDecimal(currentValue);
+				let priceBD: any = new BigDecimal(this.items[i].wartosc);
+				let valueBD: any = priceBD.add(amountBD).setScale(2, RoundingMode.HALF_UP());
+				currentValue = valueBD.toString();
+			}
+			else {
+				validAmount = false;
+				break;
+			}
+		}
+		if (validAmount) {
+			this.totalValue = currentValue;
+		}
+	}
+	private computeValue(item: any): void {
+		let amountBD: any = new BigDecimal(item.ko_ile);
+		let priceBD: any = new BigDecimal(item.c_cena);
+		let valueBD: any = priceBD.multiply(amountBD).setScale(2, RoundingMode.HALF_UP());
+
+		item.wartosc = Number(valueBD.toString());
+	}
+	customTrackBy(index: number, obj: any): any {
+		return index;
+	}
+	amountChange(item: any): void {
+		// nie ma gwarancji że this.amount jest typu string, jak do pola wprowadzi
+		// się liczbę to typ się zmienia
+		if (this.isValidInt(String(item.ko_ile))) {
+			this.computeValue(item);
+			this.computeTotalValue();
+		}
+	}
+	registerOrder(): void {
+		alert('TODO:'); // TODO:
+	}
+	deleteAllItems(): void {
+		alert('TODO:'); // TODO:
+	}
+	deleteItem(): void {
+		alert('TODO:'); // TODO:
 	}
 }
--- server/data-model-cart.js
+++ server/data-model-cart.js
@@ -3,7 +3,11 @@ module.exports = {
 		let client = dbConnObj.client;
 		let results = [];
 
-		let query = client.query('SELECT COUNT(*) AS rows_count FROM koszyk WHERE (ko_uz_id=$1)', [dataObj.userId]);
+		let query = client.query(`
+			SELECT COUNT(*) AS rows_count
+			FROM koszyk
+			WHERE (ko_uz_id=$1)
+		`, [dataObj.userId]);
 		query.on('row', (row) => {
 			results.push(row);
 		});
@@ -15,7 +19,15 @@ module.exports = {
 		let client = dbConnObj.client;
 		let results = [];
 
-		let query = client.query('SELECT * FROM koszyk WHERE (ko_uz_id=$1) ORDER BY ko_id', [dataObj.userId]);
+		let query = client.query(`
+			SELECT *, ROUND(ko_ile * c_cena, 2) AS wartosc
+			FROM koszyk
+			JOIN ceny ON (ko_c_id=c_id)
+			JOIN towary ON (c_t_id=t_id)
+			LEFT JOIN kategorie ON (t_kat_id=kat_id)
+			WHERE (ko_uz_id=$1)
+			ORDER BY t_nazwa
+		`, [dataObj.userId]);
 		query.on('row', (row) => {
 			results.push(row);
 		});
@@ -27,7 +39,13 @@ module.exports = {
 		let client = dbConnObj.client;
 		let results = [];
 
-		let query = client.query('SELECT * FROM koszyk WHERE (ko_uz_id=$1) AND (ko_c_id=$2) ORDER BY ko_id', [dataObj.userId, dataObj.priceId]);
+		let query = client.query(`
+			SELECT *
+			FROM koszyk
+			WHERE (ko_uz_id=$1)
+				AND (ko_c_id=$2)
+			ORDER BY ko_id
+		`, [dataObj.userId, dataObj.priceId]);
 		query.on('row', (row) => {
 			results.push(row);
 		});
@@ -39,12 +57,24 @@ module.exports = {
 		let client = dbConnObj.client;
 		let results = [];
 
-		client.query('INSERT INTO koszyk (ko_uz_id, ko_c_id, ko_ile) VALUES ($1, $2, $3)', [dataObj.userId, dataObj.priceId, dataObj.amount], (err) => {
+		client.query(`
+			INSERT INTO koszyk (
+				ko_uz_id,
+				ko_c_id,
+				ko_ile
+			) VALUES (
+				$1,
+				$2,
+				$3
+			)
+		`, [dataObj.userId, dataObj.priceId, dataObj.amount], (err) => {
 			if (err) {
 				callback(err, undefined);
 			}
 			else {
-				let query = client.query('SELECT currval(pg_get_serial_sequence(\'koszyk\', \'ko_id\')) AS id');
+				let query = client.query(`
+					SELECT currval(pg_get_serial_sequence('koszyk', 'ko_id')) AS id
+				`);
 				query.on('row', (row) => {
 					results.push(row);
 				});
@@ -56,7 +86,11 @@ module.exports = {
 	},
 	update: (dataObj, dbConnObj, callback) => {
 		let client = dbConnObj.client;
-		client.query('UPDATE koszyk SET ko_ile=$1 WHERE (ko_id=$2)', [dataObj.amount, dataObj.cartId], (err) => {
+		client.query(`
+			UPDATE koszyk SET
+				ko_ile=$1
+			WHERE (ko_id=$2)
+		`, [dataObj.amount, dataObj.cartId], (err) => {
 			if (err) {
 				callback(err, undefined);
 			}
